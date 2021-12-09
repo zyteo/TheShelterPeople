3f95b128720f880841d3fa9d8d2f29c26703fa85
Stabilo <zteo015@e.ntu.edu.sg>
2021-12-09 15:59:22 +0800

    Edited CSS for about page for a better mobile experience

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -13,12 +13,21 @@ const ContentBox = styled.div`
   align-items: center;
   grid-area: content;
   justify-content: center;
-  grid-gap: 50px;
+  grid-gap: 10px;
+  @media only screen and (max-width: 800px) {
+    width: 100%;
+    grid-gap: 5px;
+    display: inline-block;
+  }
 `;
 
 const Content1 = styled.div`
   width: 100%;
   height: 100%;
+  @media only screen and (max-width: 800px) {
+    display: flex;
+    justify-content: center;
+  }
 `;
 
 const P = styled.p`
@@ -27,12 +36,21 @@ const P = styled.p`
 `;
 
 const Content2 = styled(Content1)``;
-const Content3 = styled(Content1)``;
+const Content3 = styled(Content1)`
+@media only screen and (max-width: 800px) {
+  display: inline-block;
+  margin: 10px;
+}
+`;
 const Content4 = styled(Content1)``;
 
 const Logo = styled.img`
-    width: 80%;
-    height: 80%;
+  width: 90%;
+  height: 90%;
+  @media only screen and (max-width: 800px) {
+    width: 60%;
+    height: 60%;
+  }
 `;
 
 function About() {
@@ -42,22 +60,32 @@ function About() {
       <Container></Container>
       <ContentBox>
         <Content1></Content1>
-        <Content2><Logo src={logo}></Logo></Content2>
+        <Content2>
+          <Logo src={logo}></Logo>
+        </Content2>
         <Content3>
-          <P>This is a website run by The Shelter People to
-          update about TSP's progress at the shelter.</P>
-          <P>In the past 2 years, we have been raising awareness to animal shelters
-          by bringing the volunteering experience to our family and friends so
-          they can have a glimpse of what life is like for cats and dogs at the
-          shelter.</P>
-          <P>We have also seen the growth of the cats - from skittish to friendly,
-          wanting pats and cuddles. We have also seen death - which is
-          inevitable, comes with age and their rugged lives when they were
-          strays.</P>
-          <P>We hope to create more opportunities to connect and awareness to the
-          public on not abandoning their pets, pets are for life.
-          In turn, visits to the shelter can help the shelter cats to become
-          more sociable and gain higher chance at getting adopted.</P>
+          <P>
+            This is a website run by The Shelter People to update about TSP's
+            progress at the shelter.
+          </P>
+          <P>
+            In the past 2 years, we have been raising awareness to animal
+            shelters by bringing the volunteering experience to our family and
+            friends so they can have a glimpse of what life is like for cats and
+            dogs at the shelter.
+          </P>
+          <P>
+            We have also seen the growth of the cats - from skittish to
+            friendly, wanting pats and cuddles. We have also seen death - which
+            is inevitable, comes with age and their rugged lives when they were
+            strays.
+          </P>
+          <P>
+            We hope to create more opportunities to connect and awareness to the
+            public on not abandoning their pets, pets are for life. In turn,
+            visits to the shelter can help the shelter cats to become more
+            sociable and gain higher chance at getting adopted.
+          </P>
         </Content3>
         <Content4></Content4>
       </ContentBox>
