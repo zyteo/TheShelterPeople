430e287601e3665898121721d1840b9f8f91293e
Stabilo <zteo015@e.ntu.edu.sg>
2023-09-09 11:44:23 +0800

    adjusted user controller

--- controllers/user-ctrl.js
+++ controllers/user-ctrl.js
@@ -4,7 +4,6 @@
 require("dotenv").config();
 const pg = require("pg");
 const pool = new pg.Pool();
-// for hashing password
 const bcrypt = require("bcrypt");
 
 // Create all Users operations
@@ -23,25 +22,26 @@ const createUser = async (req, res) => {
   try {
     // first check if the username / email already exists
     // find the username
-    const { checkUsername } = await pool.query(
+    const { rows: checkUsername } = await pool.query(
       "SELECT * FROM users WHERE username = $1",
       [req.body.username]
     );
+
     // find the email
-    const { checkEmail } = await pool.query(
+    const { rows: checkEmail } = await pool.query(
       "SELECT * FROM users WHERE email = $1",
       [req.body.email]
     );
 
-    if (checkUsername !== null) {
+    if (checkUsername.length > 0) {
       return res.status(409).json({
         message: "username exists",
       });
-    } else if (checkEmail !== null) {
+    } else if (checkEmail.length > 0) {
       return res.status(409).json({ message: "email exists" });
     }
     // All good - create user
-    else if (checkUsername === null && checkEmail === null) {
+    else if (checkUsername.length === 0 && checkEmail.length === 0) {
       //overwrite the user password with the hashed password, then pass that in to our database
       req.body.password = bcrypt.hashSync(
         req.body.password,
@@ -52,7 +52,7 @@ const createUser = async (req, res) => {
 
       const { username, email, password, role } = req.body;
       const queryText = `
-      INSERT INTO users (username, email, image, password, role)
+      INSERT INTO users (username, email, password, role)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
     `;
@@ -70,9 +70,81 @@ const createUser = async (req, res) => {
       });
     }
   } catch (err) {
+    console.error(err);
     res.status(400).json({
       err,
       message: "User not created!",
+    });
+  }
+};
+
+// get user by id
+const getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      userId,
+    ]);
+
+    // if the user doesnt exist, throw error
+    if (rows.length === 0) {
+      return res.status(404).json({ success: false, error: `User not found` });
+    }
+
+    res.status(200).json({ success: true, data: rows });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err });
+  }
+};
+
+// update user
+const updateUser = async (req, res) => {
+  // if there is no req.body, return error
+  if (!req.body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  try {
+    // req.body exists, so find the user by id and then update
+    const userId = req.params.id;
+    // overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(
+      req.body.password,
+      bcrypt.genSaltSync(10)
+    );
+    const { username, email, password, role } = req.body;
+
+    const queryText = `
+      UPDATE users
+      SET username = $1, email = $2, password = $3, role = $4
+      WHERE id = $5
+      RETURNING *;
+    `;
+    // create values for insertion to database
+    const values = [username, email, password, role, userId];
+    const { rows } = await pool.query(queryText, values);
+
+    const user = rows[0];
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      id: user.id,
+      message: "User updated!",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({
+      error,
+      message: "User not updated!",
     });
   }
 };
@@ -93,17 +165,16 @@ const deleteUser = async (req, res) => {
     if (rowCount === 0) {
       return res.status(404).json({ success: false, error: `User not found` });
     }
-    res
-      .status(200)
-      .json({ success: true, data: rowCount, message: "User deleted!" });
+    res.status(200).json({ success: true, message: "User deleted!" });
   } catch (err) {
     res.status(400).json({ success: false, error: err });
   }
 };
 
-// export the modules - CRUD
-// Read has 2 (for the index page--> showing all users, and for the show page--> show particular user)
+// export the modules
 module.exports = {
   createUser,
+  getUserById,
+  updateUser,
   deleteUser,
 };
--- readme.md
+++ readme.md
@@ -10,34 +10,38 @@ Even though the volunteer group has since been disbanded as of 2023, I keep this
 
 This Animal Shelter website was refined from the prototype that was created for a project.
 
-*Extremely big* special shout out to my wonderful group mates (They the MVPs!!), without whom this website would not be possible! Check out the prototype's github [here.](https://github.com/soniasltan/Project-3---The-Shelter-People)
+_Extremely big_ special shout out to my wonderful group mates (They the MVPs!!), without whom this website would not be possible! Check out the prototype's github [here.](https://github.com/soniasltan/Project-3---The-Shelter-People)
 
 # Changelog
 
 Format based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
+
 ## [2.0.0] - Sep 2023
+
 ### Deprecated
+
 - Heroku deployment
 
 ### Changed
+
 - MERN to PERN stack
 
 ## [1.1.0] - 9 Dec 2021
 
 ### Added
+
 - Signup update: checks if username/email exists before creating user
 
 ### Changed
+
 - React router upgraded to v6
 - Adoptable cats and Unadoptable cats are separated
 - Styling changes: Edited CSS for better view on devices
 
-
-
-
 ###### Planning
+
 ```
 4 Sep 2023 - Reading up on PERN stack
 Reading on changes to be made from MERN to PERN
@@ -67,4 +71,7 @@ Read up on pg website.
 Adjust cat controller.
 
 8 Sep 2023 - Adjust user controller.
+
+9 Sep 2023 - Added read and update to user controller.
+
 ```
