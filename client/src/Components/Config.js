4a14d2c667427ee4fee9b09ee3db1f4cbf14c031
Stabilo <zteo015@e.ntu.edu.sg>
2023-10-21 10:14:13 +0800

    adjust pg ssl temporarily false

--- client/src/Components/Config.js
+++ client/src/Components/Config.js
@@ -2,7 +2,7 @@
 const Config = {
   API_URL: "http://localhost:3000/api/",
   // API_URL: "https://the-shelter-people-be.vercel.app/api/",
-  PG_POOL_SSL: true,
+  PG_POOL_SSL: false,
 };
 
 export default Config;
