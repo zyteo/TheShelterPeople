1ed1e22cb5bc55212b7752f25f9ff602cf5b2459
Stabilo <zteo015@e.ntu.edu.sg>
2023-10-21 11:21:19 +0800

    switch db to flexi server

--- client/src/Components/Config.js
+++ client/src/Components/Config.js
@@ -1,7 +1,7 @@
 // This config file was created to allow for easier switching of the API link
 const Config = {
-  API_URL: "http://localhost:3000/api/",
-  // API_URL: "https://the-shelter-people-be.vercel.app/api/",
+  // API_URL: "http://localhost:3000/api/",
+  API_URL: "https://the-shelter-people-be.vercel.app/api/",
 };
 
 export default Config;
